1af4df2eaee22c84653f6a78f7a2341d3e1bdcfc
Constanza González <44975847+ConstanzaG@users.noreply.github.com>
2019-05-19 22:56:20 -0400

    Add files via upload
    mostrar cantidad de asignaturas que tiene cada docente. 
    (versión preliminar, falta convertir a función)

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int contarFilas(char *argumento){
 }
 
 
+
 int main( int argc, char *argv[])
 {
     //argc: numero total de argumentos pasados por linea de comandos
@@ -26,6 +27,7 @@ int main( int argc, char *argv[])
     //Variable para guardar el argumento
     string argumento;
 
+    /*
     //Leyendo los argumentos por argumentos
     for (int i = 0; i < argc; i++)
     {
@@ -44,6 +46,77 @@ int main( int argc, char *argv[])
 	else if(argumento == "-c")
         {
             cout << contarFilas(argv[i + 1]) << endl;
+        }
+    }
+    */
+
+    //Funcion materia repetida
+
+    xlnt::workbook wb;
+    wb.load("/home/ubuntu/Escritorio/Cursos.xlsx");
+    int contador = 0;
+    auto ws = wb.active_sheet();
+
+	/* toda la hoja de cálculo */
+	std::vector< std::vector<std::string> > hoja_calculo;
+	std::vector< std::vector<std::string> > columna_c;
+	for (auto row : ws.rows(false)){ 
+
+		// Creando un vector nuevo solo para esta fila en la hoja de cálculo
+		vector<string> fila_simple;
+
+		for (auto cell : row){ 
+		    //Añadiendo esta celda a la fila;
+		    fila_simple.push_back(cell.to_string());
+		}
+
+		//Agregando esta fila completa al vector que almacena toda la hoja de cálculo;
+		hoja_calculo.push_back(fila_simple);
+        }
+
+	// Recorremos el vector y lo mostramos por pantalla
+
+
+	int contador_bloques = 0;
+    int existe = 0;
+    string nombre;
+    int idNumero = 0;
+    int idNumeros[500];
+    string nombreNumero;
+    for (int fila = 1; fila < hoja_calculo.size(); fila++){
+        contador_bloques = 0;
+        existe = 0;
+        nombre = hoja_calculo.at(fila).at(3);
+        nombreNumero = hoja_calculo.at(fila).at(2);
+        idNumero = stoi(nombreNumero, nullptr);
+        idNumeros[fila] = idNumero;
+        for (int i = 0 ; i < 500; i++)
+        {
+            if (idNumero == idNumeros[i+1])
+            {
+                //cout << "EXISTE ID: " << idNumeros[i] << endl;
+                existe++;
+            }
+        }
+        if (existe < 2)
+                {
+		//cout << "No existe ID: " << endl;
+	        for (int fila2 = 0; fila2 < hoja_calculo.size(); fila2++)
+	        {
+		      for (int columna2 = 0; columna2 < hoja_calculo.at(fila2).size(); columna2++)
+		      {
+
+		      }
+	            if(nombre==(hoja_calculo.at(fila2).at(3)))
+                {
+		            contador_bloques++;
+		        
+	            }
+	        }
+            //Muestra el numero de fila que aparece el profesor por primera vez
+	        cout << "NUMERO DE FILA" << fila << endl;
+            //Muestra el nombre del profesor y la cantidad de ramos
+            cout<<"Profesor: "<<nombre<<" , cantidad de ramos: "<<contador_bloques<<endl;
         }
     }
 
